20129f1746337120b11fdc0801942aa73238c3b2
Mark Potnick <mark.potnick@gmail.com>
2017-11-21 00:19:26 -0500

    Some config tweaks

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,35 +1,23 @@
 import { resolve } from 'path';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import { LoaderOptionsPlugin, optimize } from 'webpack';
-// import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
+import BabelMinifyPlugin from 'babel-minify-webpack-plugin';
 import merge from 'webpack-merge';
 
 import base from './webpack.base.config';
-import {css, scss} from './rules/index';
 
 export default merge(base, {
+  entry: {
+    ttun: './src/ttun.js',
+    'ttun.auto': './src/ttun.auto.js'
+  },
   output: {
     path: resolve(__dirname, 'dist'),
     pathinfo: false,
-    filename: 'ttun.js',
+    filename: '[name].min.js',
     library: 'ttun',
     libraryTarget: 'umd'
   },
   devtool: false,
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract(css.use.slice(1)),
-        include: css.include,
-      },
-    ]
-  },
   plugins: [
-    new LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    new optimize.UglifyJsPlugin(),
+    new BabelMinifyPlugin(),
   ],
 });
